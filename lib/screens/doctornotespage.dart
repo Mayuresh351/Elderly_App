c7a95b558334b4c77f4e7aa3fe48f3f84528319e
Mayuresh Mane <mpm351@gmail.com>
2020-07-06 20:52:10 +0530

    Added Dotor note plugin

--- lib/screens/doctornotespage.dart
+++ lib/screens/doctornotespage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:elderlyapp/components.dart';
+import 'package:elderlyapp/constants.dart';
 
 class NotesPage extends StatefulWidget {
   static String id = 'NotesPage';
@@ -11,14 +12,20 @@ class _NotesPageState extends State<NotesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Image.asset('images/ComingSoon.jpg'),
-            BottomTabBar(),
-          ],
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: kWidgetColor,
+        title: Text(
+          'Doctor\'s Notes',
+          style: TextStyle(color: Colors.white),
         ),
+      ),
+      body: SingleChildScrollView(),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add, color: Colors.white),
+          onPressed: () {
+            Navigator.pushNamed(context, '/addDoctorNote');
+          }
       ),
     );
   }
